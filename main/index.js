6febc94230a33520f8422c580a8feb397d22d11c
Pavlo Berezin <14924742+pavlo-berezin@users.noreply.github.com>
2020-11-15 15:07:11 +0200

    Added secondaries config

--- main/index.js
+++ main/index.js
@@ -4,9 +4,13 @@ const Router = require('router');
 const bodyParser = require('body-parser');
 const grpc = require('grpc');
 const shortid = require('shortid');
+const args = require('yargs').argv;
+const { promisify } = require('util');
 
 const messageProto = grpc.load(__dirname + '/../messages.proto');
-const client = new messageProto.MessageService('localhost:50051', grpc.credentials.createInsecure());
+const secondaries = Array.isArray(args.secondary) ? args.secondary : [args.secondary];
+
+const grpcClients = secondaries.map(url => new messageProto.MessageService(url, grpc.credentials.createInsecure()));
 
 const getDeadline = (ttl = 5000) => new Date(Date.now() + ttl);
 
@@ -32,34 +36,18 @@ router.post('/messages', (req, res) => {
   ids.add(message.id);
   messages.push(message);
 
-  client.insert(message, { deadline: getDeadline() }, (error, reply) => {
-    if (!error) {
-      console.log('success', reply);
-    } else {
-      console.log(error);
-    }
+  Promise.all(
+    grpcClients
+      .map(client => promisify(client.insert.bind(client)))
+      .map(insert => insert(message, { deadline: getDeadline() }))
+  ).then((a) => {pr
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(message));
   })
-
-
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify(message));
 })
 
-const server = http.createServer(function(req, res) {
+const server = http.createServer(function (req, res) {
   router(req, res, finalhandler(req, res))
 })
- 
+
 server.listen(3000)
-
-// let message = {
-//   id: shortid.generate(),
-//   text: 'Hello'
-// }
-
-// client.insert(message, { deadline: getDeadline() }, (error, reply) => {;
-//   if (!error) {
-//     console.log('success', reply);
-//   } else {
-//     console.log(error);
-//   }
-// })
