87bce8992138f8b19d751372b359f5d66c410e8f
Pavlo Berezin <14924742+pavlo-berezin@users.noreply.github.com>
2020-12-06 20:41:02 +0200

    Added write concern.

--- main/index.js
+++ main/index.js
@@ -11,7 +11,38 @@ const messageProto = grpc.load(__dirname + '/../messages.proto');
 const secondaries = Array.isArray(args.secondary) ? args.secondary : [args.secondary];
 const grpcClients = secondaries.map(url => new messageProto.MessageService(url, grpc.credentials.createInsecure()));
 
+const DEFAULT_CONCERN = 2;
+
 const getDeadline = (ttl = 5000) => new Date(Date.now() + ttl);
+
+const promisesWithConcern = (promises, concernN) => {
+  return new Promise((resolve, reject) => {
+    let completed = 0;
+    let resolved = 0;
+
+    for (let promise of promises) {
+      promise.then(r => {
+        resolved++;
+
+        if (resolved >= concernN) {
+          resolve(true);
+        }
+      }).catch(e => {
+        console.log(e);
+      }).finally(() => {
+        completed++;
+
+        if (completed === promises.length) {
+          reject('All promises completed but concern not achieved');
+        }
+      })
+    }
+
+    if (concernN === 0) {
+      resolve(true);
+    }
+  });
+};
 
 const messages = [];
 const ids = new Set();
@@ -27,6 +58,8 @@ router.get('/messages', (req, res) => {
 
 router.post('/messages', (req, res) => {
   const { text } = req.body;
+  const concernN = req.headers.write_concern || DEFAULT_CONCERN;
+
   const message = {
     id: shortid.generate(),
     text
@@ -35,13 +68,23 @@ router.post('/messages', (req, res) => {
   ids.add(message.id);
   messages.push(message);
 
-  Promise.all(
+  promisesWithConcern(
     grpcClients
       .map(client => promisify(client.insert.bind(client)))
       .map(insert => insert(message, { deadline: getDeadline() }))
+      .map(p => p.then(({ status }) => {
+        if (status !== 'ok') {
+          throw new Error('status not ok');
+        }
+      })),
+    concernN - 1
   ).then(() => {
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify(message));
+  }).catch((e) => {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(e));
   })
 })
 
